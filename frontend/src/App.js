7ca5f6c7e5eb753c00075b3676a79ee4afd9886c
AJ Gambino <66279789+ajGambino@users.noreply.github.com>
2024-05-31 18:39:44 -0400

    vercel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -54,7 +54,7 @@ function App() {
 
   useEffect(() => {
     filterData(searchTerm, selectedTeams, selectedPositions, selectedDivisions);
-  }, [searchTerm, selectedTeams, selectedPositions, selectedDivisions]);
+  }, [searchTerm, selectedTeams, selectedPositions, selectedDivisions, filterData]);
 
   const handleSort = (key) => {
     let direction = 'ascending';
