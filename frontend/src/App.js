1ba7bf87eeaa314b8ba3a27b5aefbb798e62d7ec
AJ Gambino <66279789+ajGambino@users.noreply.github.com>
2024-05-31 17:18:02 -0400

    connect FE and BE

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ function App() {
   const [sortConfig, setSortConfig] = useState({ key: '', direction: '' });
 
   useEffect(() => {
-    fetch('http://127.0.0.1:5000/api/draft-table')
+    fetch('/api/draft-table')
       .then(response => response.json())
       .then(data => {
         setData(data);
