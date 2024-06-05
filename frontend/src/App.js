10f0dd2bb079b18f044bd0c30c6b670fa8dc46d7
AJ Gambino <66279789+ajGambino@users.noreply.github.com>
2024-06-05 10:24:28 -0400

    styles/adp update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,7 +21,7 @@ function App() {
   const [excludeZeroExposure, setExcludeZeroExposure] = useState(false);
 
   useEffect(() => {
-    fetch("https://bestball-draft-aid.onrender.com/api/draft-table")
+    fetch(process.env.REACT_APP_API_URL)
       .then(response => response.json())
       .then(data => {
         setData(data);
